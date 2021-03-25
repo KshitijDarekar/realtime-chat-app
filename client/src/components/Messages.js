11b24606ac6fac7b306f9d15f9c4868c9e5d5150
Kshitij Darekar <kshitijdarekar13@gmail.com>
2021-03-25 17:47:27 +0530

    feat: Infobar , Input , Message,Messages,TextContainer

---
+++ client/src/components/Messages.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Message from './Message.js';
+
+const Messages = ({ messages, name }) => (
+  <div className="messages">
+    {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
+  </div>
+);
+
+export default Messages;
