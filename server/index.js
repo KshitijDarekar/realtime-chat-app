75f80411a7dfa7b395d3a1e1ce69ca0c8b5bc27b
Kshitij Darekar <kshitijdarekar13@gmail.com>
2021-03-25 17:43:21 +0530

    updated  and added socket connection for room

--- server/index.js
+++ server/index.js
@@ -3,26 +3,55 @@ const app= express();
 const server = require('http').Server(app)
 const io = require('socket.io')(server) ;
 const router = require('./router');
+
+const cors = require('cors');
+
 app.use(router);
+app.use(cors());
 
-io.on("connection", socket => {
-    
-    console.log('new Connection');
-    
-    socket.on("new-user",({name,room}) =>{
-        console.log(name,room);
-        
-      })
-    
-    socket.on('disconnect',()=>{
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
-        console.log("User disconnected");
-      })
 
-      
+const rooms = { name:{} };
+
+io.on("connection", (socket) => {
+   
+
+    socket.on('join', ({ name, room }, callback) => {
+      const { error, user } = addUser({ id: socket.id, name, room });
+  
+      if(error) return callback(error);
+  
+      socket.join(user.room);
+  
+      socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+      socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+  
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+  
+      callback();
+    });
+  
+    socket.on('sendMessage', (message, callback) => {
+      const user = getUser(socket.id);
+  
+      io.to(user.room).emit('message', { user: user.name, text: message });
+  
+      callback();
+    });
+  
+    socket.on('disconnect', () => {
+      const user = removeUser(socket.id);
+  
+      if(user) {
+        io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+      }
+    })
+     
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4050;
 const ip = process.env.IP || "0.0.0.0";
 server.listen(port,ip ,()=>{
     console.log("THe  server Has started");
